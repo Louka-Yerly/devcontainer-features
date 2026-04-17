8b830af4a8520a9afa2863c2225e51fcbb03f40c
Louka Yerly <louka.yerly@univ-grenoble-alpes.fr>
2026-04-17 21:00:10 +0200

    feat: intial version

---
+++ src/neovim-essentials/installers/ubuntu-00.00-50.00.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+apt update
+
+env
+
+pkgs=()
+
+if [ $GIT = "true" ]; then
+  pkgs+=("git")
+fi
+
+if [ $BUILD_ESSENTIAL = "true" ]; then
+  pkgs+=("build-essential")
+fi
+
+if [ $WGET = "true" ]; then
+  pkgs+=("wget")
+fi
+
+if [ $CURL = "true" ]; then
+  pkgs+=("curl")
+fi
+
+if [ $PYTHON3 = "true" ]; then
+  pkgs+=("python3")
+fi
+
+if [ $PIP3 = "true" ]; then
+  pkgs+=("python3-pip")
+fi
+
+if [ $PYTHON_IS_PYTHON3 = "true" ]; then
+  pkgs+=("python-is-python3")
+fi
+
+if [ $UNZIP = "true" ]; then
+  pkgs+=("unzip")
+fi
+
+echo "${pkgs[@]}"
+
+apt install -y "${pkgs[@]}"
+
+if [ $RUST = "true" ]; then
+    curl https://sh.rustup.rs -sSf | sh
+
+    if [ $RIPGREP = "true" ]; then
+        cargo install ripgrep
+    fi
+
+    if [ $FDFIND = "true" ]; then
+        cargo install fd
+    fi
+fi
